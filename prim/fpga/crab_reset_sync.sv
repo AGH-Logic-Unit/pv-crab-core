09cbcf7fe4917a785faf779063fd03622f9cad4d
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2026-08-08 00:03:09 +0000

    feat: add reset synchronizer prymitive

---
+++ prim/fpga/crab_reset_sync.sv
@@ -0,0 +1,24 @@
+// Copyright (c) 2026 AGH University of Krakow
+// Developed by AGH Logic Unit
+// SPDX-License-Identifier: Apache-2.0
+
+module crab_reset_sync (
+  input  logic clk_i,
+  input  logic rst_ni,
+  output logic sys_rst_no
+);
+
+  // ASYNC_REG for enforcing close placement
+  (* ASYNC_REG = "TRUE" *) logic [1:0] sync_reg_r;
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      sync_reg_r <= 2'b00;
+    end else begin
+      sync_reg_r <= {sync_reg_r[0], 1'b1};
+    end
+  end
+
+  assign sys_rst_no = sync_reg_r[1];
+
+endmodule : crab_reset_sync
